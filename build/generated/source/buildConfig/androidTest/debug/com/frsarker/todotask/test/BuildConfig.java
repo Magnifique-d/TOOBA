2d4291e531320c98070ab474f7ef0142de144c4d
Muhammad Hadi <40774340+Magnifique-d@users.noreply.github.com>
2021-04-21 03:41:29 +0500

    Update BuildConfig.java

--- build/generated/source/buildConfig/androidTest/debug/com/frsarker/todotask/test/BuildConfig.java
+++ build/generated/source/buildConfig/androidTest/debug/com/frsarker/todotask/test/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.frsarker.todotask.test;
+package com.frsarker.Tooba.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.frsarker.todotask.test";
+  public static final String APPLICATION_ID = "com.frsarker.Tooba.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
